3a9d4469360448dd34a43ccfee8475ea3f4228ee
Ariel V Lima <arielflima@gmail.com>
2020-06-09 23:06:35 -0300

    create project with all dependencies, the first cap done

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+
+  return res.json(appointments);
+});
+
+appointmentRouter.post('/', (req, res) => {
+  const { provider, date } = req.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
+  return res.json(appointment);
+});
+
+export default appointmentRouter;
