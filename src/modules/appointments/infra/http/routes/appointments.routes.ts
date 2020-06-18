0e404b4782dd260f47b0ff60ffc4d4579c31eb12
Ariel V Lima <arielflima@gmail.com>
2020-06-18 16:42:16 -0300

    refactoring routes and appling controllers with it

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,36 +1,14 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+
 const appointmentRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
-// appointmentRouter.get('/', async (req, res) => {
-//   console.log(req.user);
-
-//   const appointments = await appointmentsRepository.find();
-
-//   return res.json(appointments);
-// });
-
-appointmentRouter.post('/', async (req, res) => {
-  const { provider_id, date } = req.body;
-
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    provider_id,
-    date: parsedDate,
-  });
-
-  return res.json(appointment);
-});
+appointmentRouter.post('/', appointmentsController.create);
 
 export default appointmentRouter;
