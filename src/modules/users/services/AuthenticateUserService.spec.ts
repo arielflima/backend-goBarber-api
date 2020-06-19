81e956a68411d4ce28902cfb32e06aea4112ae48
Ariel V Lima <arielflima@gmail.com>
2020-06-19 17:43:30 -0300

    inversion and injection dependecy with providers and fakes to test created

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,14 +1,21 @@
 import AppError from '@shared/errors/AppError';
+
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
-
+import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
 
-describe('CreateUser', () => {
-  it('should to be able to create a new user', async () => {
+describe('AuthenticateUser', () => {
+  it('should to be able to authenticate', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
+
     const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
       fakeUsersRepository,
       fakeHashProvider,
     );
@@ -19,28 +26,12 @@ describe('CreateUser', () => {
       password: '123123123',
     });
 
-    expect(user).toHaveProperty('id');
-  });
-  it('should not be able to create a new user with same email from another', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUser = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
-    await createUser.execute({
-      name: 'John Doe',
+    const response = await authenticateUser.execute({
       email: 'johndoe@example.com',
       password: '123123123',
     });
 
-    expect(
-      createUser.execute({
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: '123123123',
-      }),
-    ).rejects.toBeInstanceOf(AppError);
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
   });
 });
