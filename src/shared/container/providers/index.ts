31115209e111ae6c4336d1e52e900c79c90e52ca
Ariel V Lima <arielflima@gmail.com>
2020-06-22 19:37:56 -0300

    appling TDD

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,7 +1,7 @@
 import { container } from 'tsyringe';
 
-import IStorageProvider from './StorageProviders.ts/models/IStorageProvider';
-import DiskStorageProvider from './StorageProviders.ts/implementations/DiskStorageProvider';
+import IStorageProvider from './StorageProviders/models/IStorageProvider';
+import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
