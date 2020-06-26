079201290dc195f7fb529d6d227963738eb682e2
Ariel V Lima <arielflima@gmail.com>
2020-06-26 18:54:06 -0300

    Route and Controller to profile feature

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -36,6 +36,16 @@ describe('UpdateProfile', () => {
     await expect(updatedUser.email).toBe('johntrhee@example.com');
   });
 
+  it('should not be able show the profile from non-existing user', async () => {
+    expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'Test',
+        email: 'test@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should not be able to change to another user email', async () => {
     await fakeUsersRepository.create({
       name: 'Joao',
