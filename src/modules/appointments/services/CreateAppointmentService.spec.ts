2247693899684f5496a3aa6d25e815be0f0fa3d8
Ariel V Lima <arielflima@gmail.com>
2020-07-01 13:58:41 -0300

    user creation done

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -18,24 +18,27 @@ describe('CreateAppointment', () => {
   it('should to be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
-      provider_id: '123123123',
+      user_id: '333333',
+      provider_id: '333333',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('123123123');
+    expect(appointment.provider_id).toBe('333333');
   });
   it('should not be able to create two appointments on the same time', async () => {
     const appointmentDate = new Date(2020, 4, 10, 11);
 
     await createAppointment.execute({
       date: appointmentDate,
-      provider_id: '123123123',
+      provider_id: '333333',
+      user_id: '333333',
     });
 
     expect(
       createAppointment.execute({
         date: appointmentDate,
-        provider_id: '123123123',
+        provider_id: '333333',
+        user_id: '333333',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
