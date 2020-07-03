9f5ab5676f763b62393f1878836b7eaa49945a4a
Ariel V Lima <arielflima@gmail.com>
2020-07-02 21:34:47 -0300

    show schedule feature done

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -4,11 +4,15 @@ import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAut
 
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
 
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
+
 const appointmentRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
 appointmentRouter.post('/', appointmentsController.create);
+appointmentRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentRouter;
