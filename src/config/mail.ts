ea40f42cab76ce6cb76c377dd93ab43103c5e84c
Ariel V Lima <arielflima@gmail.com>
2020-07-10 15:37:38 -0300

    aws ses configured to email sending

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,23 @@
+import { addMinutes } from 'date-fns';
+
 interface IMailConfig {
   driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'admin@admin.com',
+      name: 'Julia dos los hermanos',
+    },
+  },
 } as IMailConfig;
--- src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -1,17 +1,26 @@
-// import nodemailer, { Transporter } from 'nodemailer/lib/ses-transport';
+import nodemailer, { Transporter } from 'nodemailer';
 import { injectable, inject } from 'tsyringe';
+import mailConfig from '@config/mail';
+import aws from 'aws-sdk';
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 import IMailProvider from '../models/IMailProvider';
 import ISendMailDTO from '../dtos/ISendMailDTO';
 
 @injectable()
 export default class SESMailProvider implements IMailProvider {
-  // private client: Transporter;
+  private client: Transporter;
 
   constructor(
     @inject('MailTemplateProvider')
     private mailTemplateProvider: IMailTemplateProvider,
-  ) {}
+  ) {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+        region: 'us-east-2',
+      }),
+    });
+  }
 
   public async sendMail({
     to,
@@ -19,6 +28,18 @@ export default class SESMailProvider implements IMailProvider {
     from,
     templateData,
   }: ISendMailDTO): Promise<void> {
-    console.log('Funcionou');
+    const { name, email } = mailConfig.defaults.from;
+    await this.client.sendMail({
+      from: {
+        name: from?.name || name,
+        address: from?.email || email,
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
+    });
   }
 }
