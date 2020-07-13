b857f605ddca1e1ac1604683d2a51874ade298b0
Ariel V Lima <arielflima@gmail.com>
2020-07-13 17:52:31 -0300

    implementation to amazon S3 upload avatar

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -13,7 +13,7 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const usersRouter = Router();
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarControler();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
   '/',
